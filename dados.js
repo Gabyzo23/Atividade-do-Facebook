a8ca449e255aeaf583f2c3dd4927aa8cc1e37125
Gabyzo23 <165839207+Gabyzo23@users.noreply.github.com>
2024-04-02 17:53:18 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,7 @@
+'name';"jack",https://www.google.com/url?sa=i&url=https%3A%2F%2Fjamescameronstitanic.
+'photo';https://static.wikia.nocookie.net/jamescameronstitanic/images/c/c6/Untitledaksjk.png/revision/latest?cb=20130918130638
+'idade'; "21"
+'pai'; "Roberto"
+'mae'; "Maria"
+'sexo'; "Masculino"
+'sobremim'; "Tenho 21 anos, sou calmo e elegante."
